ebb568db9bc18d32a528a0029cfbbb4567ee83e9
Saadtronics <drsaakot@gmail.com>
2017-02-07 17:54:05 -0600

    Update IPN to include sample code for order status

--- example_rocketr_ipn.php
+++ example_rocketr_ipn.php
@@ -8,6 +8,27 @@ abstract class rocketrPaymentMethods
     const STRIPE = 4;
 }
 
+abstract class rocketrOrderStatus {
+    const TIMED_OUT = -1; //This means the buyer did not pay
+    const NEW_ORDER = 0; //Order was just created, the buyer may or may not pay
+    const WAITING_FOR_PAYMENT = 1; //This is exclusive for cryptocurrency payments, this means we are waiting for confirmations
+    const ERROR_PARTIAL_PAYMENT_RECEIVED = 2; //the buyer only paid a partial amount
+    const FULL_PAYMENT_RECEIVED = 3; //this order status signifies that the product delivery failed (e.g. b/c the buyers email was incorrect or out of stock)
+    const PRODUCT_DELIVERED = 4; // AKA success. This signifies product email delivery
+    const REFUNDED = 5; //The order was refunded
+        
+    const UNKNOWN_ERROR = 6;
+    
+    const PAYPAL_PENDING = 8;
+    const PAYPAL_OTHER = 9; //if a paypal dispute is favored to the seller, this is the order status.
+    const PAYPAL_REVERSED = 10; //buyer disputed via paypal
+    
+    const STRIPE_AUTO_REFUND = 20;
+    const STRIPE_DECLINED = 21;
+    const STRIPE_DISPUTED = 22;
+    
+}
+
 if(!isset($_POST) || sizeof($_POST) === 0 || !isset($_SERVER['HTTP_IPN_HASH'])) {    
     die('Received Invalid IPN ');
 }
@@ -29,12 +50,17 @@ $invoice_amount_usd = floatval($_POST['invoice_amount_usd']);
 $quantity = intval($_POST['quantity']);
 $purchased_at = $_POST['purchased_at'];
 $txn_id = $_POST['txn_id']; //note this can represent different things, paypal's transaction id, btc/eth blockchain txid, perfect momey id etc
+$status = intval($_POST['status']);
 
 /**
  * You can process the IPN below.
  * For example:
- *  if($payment_method === rocketrPaymentMethods::PAYPAL) {
- *      sendBuyerEmail('Thanks for paying with Paypal');
+ *  if($status === rocketrOrderStatus::PRODUCT_DELIVERED) {
+ *  	if($payment_method === rocketrPaymentMethods::PAYPAL) {
+ *      	sendBuyerEmail('Thanks for paying with Paypal');
+ *  	}
+ *  } else if($status === rocketrOrderStatus::PAYPAL_REVERSED) {
+ *  	//E.G Here you can revoke the license.
  *  }
  */
  
